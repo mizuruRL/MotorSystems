ba0bb275cb24fe87627fd25adbdaf880b5fa0a5a
Tiago Branco <tiagomcbranco@hotmail.com>
2022-03-15 16:04:03 +0000

    test

---
+++ MotorSystemsApp/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace MotorSystemsApp.Models
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Brand { get; set; }
+        public double Price { get; set; }
+        public double QuantityNeeded { get; set; }
+        public double AvailableQuantity { get; set; }
+    }
+}
